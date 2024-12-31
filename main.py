29aaf1b1305c58f76cbffa94ee3824ab5950952a
redhawk5200 <abdulwahab052001@gmail.com>
2024-12-31 17:37:17 +0500

    new route

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 from dotenv import load_dotenv
 from core.config import get_config
-from schema import schemas
+from schema.schemas import User
 from fastapi import FastAPI
 from typing import List
+from uuid import uuid4
 
 load_dotenv()
 
-
-
 app = FastAPI()
 
-db: List[User]
+db: List[User]= [
+    User(
+        id=uuid4(),
+        conversation_id=uuid4(),
+        name="John Doe",
+        email="john@gmail.com",
+        is_active=True
+    ),
+    User(
+        id=uuid4(),
+        conversation_id=uuid4(),
+        name="Mary Doe",
+        email="mary@gmail.com",
+        is_active=False
+    )
+]
 
 @app.get("/")
-
 async def root():
     return {"Hello": "Mundo"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
 
 
 
